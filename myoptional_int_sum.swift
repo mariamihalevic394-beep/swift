b3c91d15f2685dc49048b939deb89a622bb0d240
mariamihalevic394-beep <mariamihalevic394@gmail.com>
2026-06-23 14:33:57 +0300

    Fix syntax issues and improve MyOptional implementation

--- myoptional_int_sum.swift
+++ myoptional_int_sum.swift
@@ -5,8 +5,8 @@ enum MyOptional<T> {
     case none
 }
 
-func sum(_ first: MyOPtional<Int>,
-_ second: MyOptional<>Int) -> MyOptional<Int> {
+func sum(_ first: MyOptional<Int>,
+_ second: MyOptional<Int>) -> MyOptional<Int> {
     switch (first, second) {
         case (.some(let value1), .some(let value2)):
         return .some(value1 + value2)
@@ -16,19 +16,19 @@ _ second: MyOptional<>Int) -> MyOptional<Int> {
     }
 }
 
-let number1: MyOPtional<Int> = .some(10)
-let number2: MyOPtional<Int> = .some(20)
+let number1: MyOptional<Int> = .some(10)
+let number2: MyOptional<Int> = .some(20)
 
 let result1 = sum(number1, number2)
 
-switch result1 = {
+switch result1 {
     case .some(let value):
         print("Сума = \(value)")
     case .none:
         print("Неможливо обчислити суму")
 }
 
-let number3: MyOPtional<Int> = .none
+let number3: MyOptional<Int> = .none
 let result2 = sum(number1, number3)
 
 switch result2 {
